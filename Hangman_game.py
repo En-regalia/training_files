a92f2c3ca52842e4ba614eb491a700830b705e1f
En-regalia <mike.ranner1@gmail.com>
2024-04-03 15:45:52 +0100

    Name changes to remove ' ' replace with '_'

---
+++ Hangman_game.py
@@ -0,0 +1,20 @@
+# Hangman game
+
+import random
+
+def get_choices():
+  game_options =['rock', 'paper', 'scissors']
+  game_word = random.choice(game_options)
+  return game_word
+
+def guess_outcome(guess)
+  guess_char = [guess]
+  for char in guess_char
+
+player_guess = input('Please guess the charictar in the word')
+
+x = 0
+while x <= len game_word:
+  for char in player_guess
+    if char == game_word[x]:
+
